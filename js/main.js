4d58912a5917a9d9d6b182ba475c4024d693a0d9
Branden Arthur <branden.arthur13@gmail.com>
2020-07-15 07:55:56 -0600

    fixed gallery issue

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// We will extend this list later.
+const imgList = [1019, 1065, 1051, 1049, 1035]; 
+
+// Variable where we will store our img tags
+let imgTemplate = ''; 
+
+// Flex container for images
+const gallery = document.querySelector('.gallery'); 
+
+// Loop through items using forEach (available on every array)
+imgList.forEach(function(item){
+  console.log(item);
+  // Our output variable (imgTemplate) contains a gradually growing list of animal images using the addition assignment operator
+  imgTemplate += `<img src="https://picsum.photos/id/${item}/250/250" alt="Random Lorem Picsum">`;
+});
+
+// Add HTML img string to gallery container
+gallery.innerHTML = imgTemplate;
+
 const email = document.getElementById("mail");
 
 email.addEventListener("input", function (event) {
@@ -13,22 +32,3 @@ document.getElementsByName('password')[0].setAttribute("placeholder", "8 Charact
 document.getElementsByName('confirmPassword')[0].setAttribute("placeholder", "8 Character Min");
 document.getElementsByName('fullName')[0].setAttribute("placeholder", "First Last");
 
-
-    // We will extend this list later.
-    const imgList = [1019, 1065, 1051, 1049, 1035]; 
-
-    // Variable where we will store our img tags
-    let imgTemplate = ''; 
-
-    // Flex container for images
-    const gallery = document.querySelector('.gallery'); 
-
-    // Loop through items using forEach (available on every array)
-    imgList.forEach(function(item){
-      console.log(item);
-      // Our output variable (imgTemplate) contains a gradually growing list of animal images using the addition assignment operator
-      imgTemplate += `<img src="https://picsum.photos/id/${item}/250/250" alt="Random Lorem Picsum">`;
-    });
-
-    // Add HTML img string to gallery container
-    gallery.innerHTML = imgTemplate;
